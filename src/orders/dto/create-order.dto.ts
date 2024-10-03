921b8dd13f77ebdb31edb90080fc88a135a01ab9
Hector Lopez <h.lopez@rimith.com>
2024-10-03 16:59:26 -0500

    refactoring CreateOrderDto, add OrderItemDto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,24 +1,14 @@
-import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
-import { OrdersStatus, OrdersStatusList } from "../enum/order.enum";
+import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+import { OrderItemDto } from "./order-item.dto";
 
 
 export class CreateOrderDto {
 
-    @IsNumber()
-    @IsPositive()
-    totalAmount: number;
+    @IsArray()
+    @ArrayMinSize(1)
+    @ValidateNested({ each: true })
+    @Type( ()=> OrderItemDto)
+    items: OrderItemDto[]
 
-    @IsNumber()
-    @IsPositive()
-    totalItems: number;
-
-    @IsEnum(OrdersStatusList, {
-        message: `Posible status values are ${OrdersStatusList}`
-    })
-    @IsOptional()
-    status: OrdersStatus = OrdersStatus.PENDING;
-
-    @IsBoolean()
-    @IsOptional()
-    paid: boolean = false;
 }
--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -1,3 +1,4 @@
 export *from './create-order.dto';
 export *from './ordder-pagination.dto';
 export *from './status.dto';
+export *from './order-item.dto'
